006fdf2d0d8d452e22217de4cd4346b6867efb3d
MiroHann <122731952+MiroHann@users.noreply.github.com>
2024-02-04 18:56:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Map from './screens/Map';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     <Map/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0
+  },
+});
